cbda1fca0619bc791a555707742564b854fd299c
Shubham Kumar Gupta <krishkumargupta7631@gmail.com>
2026-07-26 19:59:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-  'https://nexasoul-quiz-frontend.onrender.com',
+  'https://nexasoul-srujana.onrender.com',
 ].filter(Boolean);
 
 app.use(cors({
